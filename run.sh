4f565b50eebf000f4531dc496aa3e978890363d5
Konstantins Starovoitovs <starovoitovs1@jwlogin23.juwels>
2022-03-30 17:35:06 +0200

    Slurm hotfix

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@
 #SBATCH --gres=gpu:4
 #SBATCH --account=training2203
 
-module load Stages/2022 GCC/11.2.0 OpenMPI/4.1.2 Horovod/0.24.2-Python-3.9.6
-cd /p/home/jusers/starovoitovs1/juwels/projects/makers
+module load Stages/2022 GCC/11.2.0 OpenMPI/4.1.2 Horovod/0.24.2-Python-3.9.6 Nsight-Systems
+cd /p/home/jusers/$USER/juwels/projects/makers
 
 srun nsys profile -t mpi,cuda,nvtx --mpi-impl=openmpi -s none -f true -o profiler.%q{SLURM_PROCID}.qdrep python fbsde.py
 
