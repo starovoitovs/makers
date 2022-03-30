5dbe57aedb70f9a93a2d90b9b55f16c840d14aed
Konstantins Starovoitovs <starovoitovs1@jwlogin23.juwels>
2022-03-30 17:23:27 +0200

    Slurm shell script

--- run.sh
+++ run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -l
 #SBATCH -p booster
 #SBATCH -t 02:00:00
-#SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:4
 #SBATCH --account=training2203
-#SBATCH --reservation=gpuhack22-30
 
 module load Stages/2022 GCC/11.2.0 OpenMPI/4.1.2 Horovod/0.24.2-Python-3.9.6
 cd /p/home/jusers/starovoitovs1/juwels/projects/makers
 
-env > env.txt
-srun python fbsde.py
+srun nsys profile -t mpi,cuda,nvtx --mpi-impl=openmpi -s none -f true -o profiler.%q{SLURM_PROCID}.qdrep python fbsde.py
+
